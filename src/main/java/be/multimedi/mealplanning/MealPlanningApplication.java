22c9b62ed6c0f7e02f42de0d8d49475064d1c2b6
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-24 10:04:46 +0200

    inital commit

---
+++ src/main/java/be/multimedi/mealplanning/MealPlanningApplication.java
@@ -0,0 +1,13 @@
+package be.multimedi.mealplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealPlanningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/multimedi/mealplanning/MealPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package be.multimedi.mealplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
