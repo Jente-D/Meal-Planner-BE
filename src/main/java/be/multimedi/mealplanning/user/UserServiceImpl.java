95105d9b7c9d04468e0663b4eb9f7ce75e43af18
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-27 12:13:00 +0200

    build: #4 create UserServicImpl class as a bean

---
+++ src/main/java/be/multimedi/mealplanning/user/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package be.multimedi.mealplanning.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public User registerNewUser(User user) {
+        return null;
+    }
+}
