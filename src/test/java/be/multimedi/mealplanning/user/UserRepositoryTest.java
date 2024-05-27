d3f9518b76815fc8c8c2fc6c9736508c44a0b495
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-27 15:47:25 +0200

    test: #4 create an integrationTest for the UserRepo

---
+++ src/test/java/be/multimedi/mealplanning/user/UserRepositoryTest.java
@@ -0,0 +1,27 @@
+package be.multimedi.mealplanning.user;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("integrationTest")
+@DataJpaTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepo;
+
+    @Test
+    void existsByEmail() {
+        //arrange
+        User user = new User();
+        user.setEmail("test@gmail.com");
+        //Act
+        userRepo.save(user);
+        //Assert
+        assertTrue(userRepo.existsByEmail("test@gmail.com"));
+        assertFalse(userRepo.existsByEmail("noemail@gmail.com"));
+    }
+}
