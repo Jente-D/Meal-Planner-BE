fd5b2af75f48dfd394c9cd8c26c1bfc901a45871
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-27 12:21:14 +0200

    feat: #4 create a registerNewUser logic using the user id

--- src/main/java/be/multimedi/mealplanning/user/UserServiceImpl.java
+++ src/main/java/be/multimedi/mealplanning/user/UserServiceImpl.java
@@ -1,11 +1,18 @@
 package be.multimedi.mealplanning.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepo;
+
     @Override
     public User registerNewUser(User user) {
-        return null;
+        if(user.getId() != null){
+            throw new IllegalArgumentException("Cannot save user with existing id: " + user.getId());
+        }
+        return userRepo.save(user);
     }
 }
