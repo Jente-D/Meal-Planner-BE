50044aba4f2a034b8cd6ceced26ea4eec56692ba
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-27 15:24:12 +0200

    feat: #4 adjust registerNewUser by using DTO instead of User

--- src/main/java/be/multimedi/mealplanning/user/UserService.java
+++ src/main/java/be/multimedi/mealplanning/user/UserService.java
@@ -1,6 +1,5 @@
 package be.multimedi.mealplanning.user;
 
 public interface UserService {
-    //TODO: omvormen userDTO naar user al dan niet in controller of service > consistent zijn
-    User registerNewUser(registerUserDto registerUserDto);
+    User registerNewUser(UserRegistrationDto UserRegistrationDto);
 }
