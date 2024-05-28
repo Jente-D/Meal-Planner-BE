d31ed4ddfd5727bf8484f640c1b82a1002fd61e3
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-28 17:48:19 +0200

    feat: #5 add UserLoginDto

---
+++ src/main/java/be/multimedi/mealplanning/user/UserLoginDto.java
@@ -0,0 +1,25 @@
+package be.multimedi.mealplanning.user;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class UserLoginDto {
+//    TODO: weer dezelfde validator? of toch iets anders?
+//    TODO: wat is dan het verschil met de registeredDTO of is dit voor toekomst project groei al goed zodat DTO maar in 1 senario gebruikt word
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+
+    private static User convertToEntity(UserLoginDto dto){
+        return User.builder()
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .build();
+    }
+}
