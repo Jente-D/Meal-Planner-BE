594464d8f770b0e6494e38cee49320a1582ecbc1
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-28 15:55:33 +0200

    feat: #4 addjust validation of email isn,t the responisbility of the service

--- src/main/java/be/multimedi/mealplanning/user/UserDatabaseService.java
+++ src/main/java/be/multimedi/mealplanning/user/UserDatabaseService.java
@@ -17,20 +17,11 @@ public class UserDatabaseService implements UserService {
 
     @Override
     public User registerNewUser(UserRegistrationDto userRegistrationDto) {
-//        TODO: extra uitleg rond validatie gebruik makend van je annotaties
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-        Set<ConstraintViolation<UserRegistrationDto>> violations = validator.validate(userRegistrationDto);
-        if (!violations.isEmpty()) {
-            String errorMessage = violations.iterator().next().getMessage();
-            throw new IllegalArgumentException(errorMessage);
-        }
         if(userRepo.existsByEmail(userRegistrationDto.getEmail())){
             throw new EntityExistsException("Email taken: " + userRegistrationDto.getEmail());
         }
-//        TODO: zet je u pasworodencodere al op de dto of pas op de user? (ik zou denken zo snel mogelijk)
-        userRegistrationDto.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
         User user = userRegistrationDto.convertToEntity(userRegistrationDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 }
