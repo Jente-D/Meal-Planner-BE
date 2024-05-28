514d65b44e500a3484f7a2c89ef824a316ba997a
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-28 11:38:58 +0200

    feat: #4 implement passwordEncoder to registerNewUser in UserDatabaseService

--- src/main/java/be/multimedi/mealplanning/user/UserDatabaseService.java
+++ src/main/java/be/multimedi/mealplanning/user/UserDatabaseService.java
@@ -3,6 +3,7 @@ package be.multimedi.mealplanning.user;
 import jakarta.persistence.EntityExistsException;
 import jakarta.validation.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
@@ -12,21 +13,24 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class UserDatabaseService implements UserService {
     private final UserRepository userRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
-    public User registerNewUser(UserRegistrationDto UserRegistrationDto) {
+    public User registerNewUser(UserRegistrationDto userRegistrationDto) {
 //        TODO: extra uitleg rond validatie gebruik makend van je annotaties
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         Validator validator = factory.getValidator();
-        Set<ConstraintViolation<UserRegistrationDto>> violations = validator.validate(UserRegistrationDto);
+        Set<ConstraintViolation<UserRegistrationDto>> violations = validator.validate(userRegistrationDto);
         if (!violations.isEmpty()) {
             String errorMessage = violations.iterator().next().getMessage();
             throw new IllegalArgumentException(errorMessage);
         }
-        if(userRepo.existsByEmail(UserRegistrationDto.getEmail())){
-            throw new EntityExistsException("Email taken: " + UserRegistrationDto.getEmail());
+        if(userRepo.existsByEmail(userRegistrationDto.getEmail())){
+            throw new EntityExistsException("Email taken: " + userRegistrationDto.getEmail());
         }
-        User user = UserRegistrationDto.convertToEntity(UserRegistrationDto);
+//        TODO: zet je u pasworodencodere al op de dto of pas op de user? (ik zou denken zo snel mogelijk)
+        userRegistrationDto.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
+        User user = userRegistrationDto.convertToEntity(userRegistrationDto);
         return userRepo.save(user);
     }
 }
