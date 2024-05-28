312259242c4add506166953955e2467641e68368
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-28 17:20:03 +0200

    feat: #4 adjust UserRegistrationDto and USer by adding Pattern regex for password validation

--- src/main/java/be/multimedi/mealplanning/user/User.java
+++ src/main/java/be/multimedi/mealplanning/user/User.java
@@ -17,12 +17,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column (nullable = false, unique = true)
     @NotBlank
     @Email(message="Please provide a valid email address")
     @Pattern(regexp="^[a-zA-Z0-9.\\-_]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$", message="Please provide a valid email address")
+    @Column (nullable = false, unique = true)
     private String email;
     @NotBlank
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[.?!]).{12,}", message = "is not a valid password. Your password must be at least 12 characters long and include at least one lowercase letter, one uppercase letter, one digit, and one special character (., ?, !).")
     @Column (nullable = false)
     private String password;
 }
