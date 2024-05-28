dbda3a66269efa419ae1c7d3dad4fbf13c8d12d1
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-28 12:10:20 +0200

    feat: #4 add UserDetailsServiceImpl to integrade spring security

---
+++ src/main/java/be/multimedi/mealplanning/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package be.multimedi.mealplanning.security;
+
+import be.multimedi.mealplanning.user.User;
+import be.multimedi.mealplanning.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepo;
+
+    //TODO: De naam loadUserByUsername kan niet veranderd worden > voorinstelling spring? maar wat als ik wil kijken of gebruiker bestaat met email > onlogisch of?
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        User user = userRepo.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException(email));
+        UserDetails userDetails = new UserDetailsImpl(user);
+        return userDetails;
+    }
+}
