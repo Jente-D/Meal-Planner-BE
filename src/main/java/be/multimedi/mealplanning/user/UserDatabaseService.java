5719ce5a6cb609384765986fa3dc0787e1b55636
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-28 08:38:05 +0200

    feat: #4 adjust registerNewUser adn add validation of Email

--- src/main/java/be/multimedi/mealplanning/user/UserDatabaseService.java
+++ src/main/java/be/multimedi/mealplanning/user/UserDatabaseService.java
@@ -1,8 +1,12 @@
 package be.multimedi.mealplanning.user;
 
 import jakarta.persistence.EntityExistsException;
+import jakarta.validation.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +15,14 @@ public class UserDatabaseService implements UserService {
 
     @Override
     public User registerNewUser(UserRegistrationDto UserRegistrationDto) {
+//        TODO: extra uitleg rond validatie gebruik makend van je annotaties
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<UserRegistrationDto>> violations = validator.validate(UserRegistrationDto);
+        if (!violations.isEmpty()) {
+            String errorMessage = violations.iterator().next().getMessage();
+            throw new IllegalArgumentException(errorMessage);
+        }
         if(userRepo.existsByEmail(UserRegistrationDto.getEmail())){
             throw new EntityExistsException("Email taken: " + UserRegistrationDto.getEmail());
         }
