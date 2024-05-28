4c19ff9ee05008ea54f8e747b0cd81faf328a21c
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-28 17:48:46 +0200

    feat: #5 adjust SecurityConfig by adding AuthenticationManager bean

--- src/main/java/be/multimedi/mealplanning/security/SecurityConfig.java
+++ src/main/java/be/multimedi/mealplanning/security/SecurityConfig.java
@@ -2,7 +2,9 @@ package be.multimedi.mealplanning.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,7 +19,10 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(a -> a.anyRequest().permitAll())
