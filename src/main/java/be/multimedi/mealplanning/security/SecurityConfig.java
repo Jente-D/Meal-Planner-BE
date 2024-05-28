d1ba042e119dfdcbf92f5a53591d80425e3634c3
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-28 16:00:00 +0200

    feat: #4 resole to-do's

--- src/main/java/be/multimedi/mealplanning/security/SecurityConfig.java
+++ src/main/java/be/multimedi/mealplanning/security/SecurityConfig.java
@@ -2,8 +2,13 @@ package be.multimedi.mealplanning.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -13,4 +18,12 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(a -> a.anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
 }
