ea42abf58b264448a6125efe413869f41b3b09e6
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-29 12:28:06 +0200

    test: #4 adjust 00-schema to test and create database with postman

--- data/00-schema.sql
+++ data/00-schema.sql
@@ -1,8 +1,7 @@
 DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users (
-    id int generated always as identity,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
+      id int generated always as identity,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      password VARCHAR(255) NOT NULL
 );
