a80dfb0a552602f9dd38d55a978f0309670e3070
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-31 11:12:45 +0200

    feat: #7 create RegistrationRequest table in database

--- data/00-schema.sql
+++ data/00-schema.sql
@@ -1,7 +1,9 @@
-DROP TABLE IF EXISTS Users;
 
-CREATE TABLE Users (
-      id int generated always as identity,
-      email VARCHAR(255) NOT NULL UNIQUE,
-      password VARCHAR(255) NOT NULL
+CREATE TABLE registration_request (
+        id SERIAL PRIMARY KEY,
+        confirmation_token VARCHAR(255),
+        created_date TIMESTAMP,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        status BOOLEAN NOT NULL
 );
