28a8a78069240f49f501b31a0ac1668596552e27
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-31 11:12:10 +0200

    feat: #7 add RegistrationRequest Entity

---
+++ src/main/java/be/multimedi/mealplanning/authentication/RegistrationRequest.java
@@ -0,0 +1,45 @@
+package be.multimedi.mealplanning.authentication;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class RegistrationRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="confirmation_token")
+    private String confirmationToken;
+
+    @Temporal(TemporalType.DATE)
+    private Date createdDate;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private boolean status;
+
+    public RegistrationRequest(UserRegistrationDto userRegistrationDto) {
+        this.email = userRegistrationDto.getEmail();
+        this.password = userRegistrationDto.getPassword();
+        createdDate = new Date();
+        confirmationToken = UUID.randomUUID().toString();
+    }
+
+    @PrePersist
+    protected void setCreationDate() {
+        createdDate = new Date();
+    }
+}
