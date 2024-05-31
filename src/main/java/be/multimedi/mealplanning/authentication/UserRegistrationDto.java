ec7a5f4000a6e7cf3a4fe021effecd7ad03d4404
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-31 16:23:34 +0200

    fix: #7 deleting all validation in UserRegistrationDto

--- src/main/java/be/multimedi/mealplanning/authentication/UserRegistrationDto.java
+++ src/main/java/be/multimedi/mealplanning/authentication/UserRegistrationDto.java
@@ -1,16 +1,11 @@
 package be.multimedi.mealplanning.authentication;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
 @AllArgsConstructor
 public class UserRegistrationDto {
-    @Email(message="Please provide a valid email address")
-    @Pattern(regexp="^[a-zA-Z0-9.\\-_\\+]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$", message="Please provide a valid email address")
     private String email;
-    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[.?!]).{12,}", message = "is not a valid password. Your password must be at least 12 characters long and include at least one lowercase letter, one uppercase letter, one digit, and one special character (., ?, !).")
     private String password;
 
     //TODO: Model mapper overwegen te gebruiken Libr of mapst... modelmapper niet compatible met record in pom toevoegen daaarvoor + apparte klasse
