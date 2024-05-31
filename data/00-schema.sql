9db58810951301c531c965d2b3e0b6c1e70f6d5e
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-31 15:26:50 +0200

    feat: #7 adjust 00-schema creating tables

--- data/00-schema.sql
+++ data/00-schema.sql
@@ -1,10 +1,16 @@
---
--- CREATE TABLE registration_request (
---         id SERIAL PRIMARY KEY,
---         confirmation_token VARCHAR(255),
---         created_date TIMESTAMP,
---         email VARCHAR(255) NOT NULL UNIQUE,
---         password VARCHAR(255) NOT NULL,
---         status BOOLEAN NOT NULL
--- );
-TRUNCATE TABLE registration_request;
+create table users(
+    id int generated always as identity,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    primary key (id)
+);
+
+CREATE TABLE registration_request (
+        id int GENERATED ALWAYS AS identity,
+        confirmation_token VARCHAR(255),
+        created_date TIMESTAMP,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        status BOOLEAN NOT NULL,
+        primary key (id)
+);
