a7646a91585d060f152c883523429602494fa7ca
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-06-04 16:40:44 +0200

    feat: #7 adjust the postmapping in AuthController to implement the request of a new user

--- src/main/java/be/multimedi/mealplanning/authentication/AuthController.java
+++ src/main/java/be/multimedi/mealplanning/authentication/AuthController.java
@@ -17,28 +17,29 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final UserService userService;
     private final AuthenticationManager authManager;
+    private final RegistrationRequestService registrationRequestService;
 
-//    @PostMapping("/register")
-//    public ResponseEntity<String> handleRegisterNewUser(@Valid @RequestBody UserRegistrationDto userDto, BindingResult br){
-//        try{
-//        if(br.hasErrors()){
-//            String errorMsg;
-//            if (br.hasFieldErrors("email")) {
-//                errorMsg = br.getFieldError("email").getDefaultMessage();
-//            } else if (br.hasFieldErrors("password")) {
-//                errorMsg = br.getFieldError("password").getDefaultMessage();
-//            } else {
-//                errorMsg = "Validation error";
-//            }
-//            throw new IllegalArgumentException(errorMsg);
-//        }}
-//        catch (IllegalArgumentException iae){
-//            System.out.println(iae.getMessage());
-//            return ResponseEntity.badRequest().build();
-//        }
-//        userService.registerNewUser(userDto);
-//        return ResponseEntity.ok("You can now login as " + userDto.getEmail());
-//    }
+    @PostMapping("/register")
+    public ResponseEntity<String> handleRegisterNewUserRequest (@Valid @RequestBody UserRegistrationDto userDto, BindingResult br){
+        try{
+        if(br.hasErrors()){
+            String errorMsg;
+            if (br.hasFieldErrors("email")) {
+                errorMsg = br.getFieldError("email").getDefaultMessage();
+            } else if (br.hasFieldErrors("password")) {
+                errorMsg = br.getFieldError("password").getDefaultMessage();
+            } else {
+                errorMsg = "Validation error";
+            }
+            throw new IllegalArgumentException(errorMsg);
+        }}
+        catch (IllegalArgumentException iae){
+            System.out.println(iae.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+        registrationRequestService.registerNewUserRequest(userDto);
+        return ResponseEntity.ok("A confimation email has been sent to " + userDto.getEmail());
+    }
 
     @PostMapping("/login")
     public ResponseEntity<String> handleLogin (@Valid @RequestBody UserLoginDto loginDto, BindingResult br){
