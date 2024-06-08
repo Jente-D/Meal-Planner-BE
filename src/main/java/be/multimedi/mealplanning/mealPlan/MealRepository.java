3aaa818d78e45012ce47b425a095a02d2f9f2930
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-06-08 19:00:26 +0200

    feat: #9 adjust mealRepo by adding and adjusting the methods

--- src/main/java/be/multimedi/mealplanning/mealPlan/MealRepository.java
+++ src/main/java/be/multimedi/mealplanning/mealPlan/MealRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MealRepository extends JpaRepository<Meal, Long> {
-    @Query("SELECT m FROM Meal m WHERE m.calories <= :calories")
-    List<Meal> findMealsByCaloriesLessThanEqual(@Param("calories") int calories);
+    @Query("SELECT m FROM Meal m WHERE m.calories >= :minCalories AND m.calories <= :maxCalories")
+    List<Meal> findMealsByCaloriesBetween(@Param("minCalories") int minCalories, @Param("maxCalories") int maxCalories);
 
     List<Meal> findMealsByMealType(MealType mealType);
-//    Optional
-    Meal findRandomMealByMealTypeAndCaloriesLessThanEqual (MealType mealType, int calories);
+
+//    Optional<Meal> findRandomMealByMealTypeAndCaloriesLessThanEqual (MealType mealType, int calories);
 }
