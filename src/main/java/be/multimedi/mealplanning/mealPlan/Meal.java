345965adfd33bde1c16e1c4bcc2debdef93e289d
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-06-08 18:43:02 +0200

    feat: #9 adjust meal by adding image url

--- src/main/java/be/multimedi/mealplanning/mealPlan/Meal.java
+++ src/main/java/be/multimedi/mealplanning/mealPlan/Meal.java
@@ -1,9 +1,15 @@
 package be.multimedi.mealplanning.mealPlan;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "meals")
 public class Meal {
@@ -14,4 +20,5 @@ public class Meal {
     @Enumerated(EnumType.STRING)
     private MealType mealType;
     private int calories;
+    private String imgUrl;
 }
